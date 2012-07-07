024f252c1416b374a4ceabeebd5bd41182015baf
Alessio Chiapperini <ale4694@gmail.com>
2012-07-07 13:05:51 +0200

    Fixed code indentation & player hitbox.

--- src/com/cnia/angelworks/ba/entity/Player.java
+++ src/com/cnia/angelworks/ba/entity/Player.java
@@ -1,4 +1,3 @@
-
 package com.cnia.angelworks.ba.entity;
 
 import it.randomtower.engine.ResourceManager;
@@ -11,16 +10,16 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 /**
- * @author CNIAngel
- * This is a simple example for a player entity class. It will show
- * movement, calling on the sprite, setting the hitbox, and collision
+ * @author CNIAngel This is a simple example for a player entity class. It will
+ *         show movement, calling on the sprite, setting the hitbox, and
+ *         collision
  */
 public class Player extends PhysicsEntity {
 
 	private int moveSpeed = 4, jumpSpeed = 11; // SUPER JUMP!!!!
 	private boolean isJumping, onGround;
 	private Image img; // an Image instance for the sprite
-	
+
 	/**
 	 * @param x
 	 * @param y
@@ -28,90 +27,93 @@ public class Player extends PhysicsEntity {
 	public Player(float x, float y) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
-		
-		img = ResourceManager.getImage("doc"); 	// Set the image to the desired sprite
-		setGraphic(img);							// Then set the sprite for the Player entity
-		
+
+		img = ResourceManager.getImage("doc"); // Set the image to the desired
+												// sprite
+		setGraphic(img); // Then set the sprite for the Player entity
+
 		// Define preset controls for the Player entity
 		define("UP", Input.KEY_UP);
-		//define("DOWN", Input.KEY_DOWN);
+		// define("DOWN", Input.KEY_DOWN);
 		define("LEFT", Input.KEY_LEFT);
 		define("RIGHT", Input.KEY_RIGHT);
-		
+
 		gravity = 0.4f;
-		
+
 		/*
-		 * Set the hitbox for the Player entity using the entity's x variable,
-		 * y variable, the width of the entity's sprite, and the height of the entity's sprite
+		 * Set the hitbox for the Player entity using the entity's x variable, y
+		 * variable, the width of the entity's sprite, and the height of the
+		 * entity's sprite
 		 */
-		setHitBox(x, y, img.getWidth(), img.getHeight());
-		
+		setHitBox(0, 0, img.getWidth(), img.getHeight());
+
 		// Set the collsion type for the entity
 		addType(PLAYER);
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see it.randomtower.engine.entity.Entity#update(org.newdawn.slick.GameContainer, int)
+	 * 
+	 * @see
+	 * it.randomtower.engine.entity.Entity#update(org.newdawn.slick.GameContainer
+	 * , int)
 	 */
 	public void update(GameContainer gc, int delta) throws SlickException {
 		super.update(gc, delta);
-		
+
 		Input input = gc.getInput();
-		
+
 		onGround = false;
-		if (collide(SOLID, x, y + 1) != null) {
-		onGround = true;
-		isJumping = false;
+		if (collide(SOLID, x, y + jumpSpeed) != null) {
+			onGround = true;
+			isJumping = false;
 		}
-		
-		
-		
-		if (check("LEFT"))  {
-			
-			if(collide(SOLID, x - 2, y) != null) {
-				
+
+		if (check("LEFT")) {
+
+			if (collide(SOLID, x - moveSpeed, y) != null) {
+
 				x -= 0;
 			} else {
 				x -= moveSpeed;
 			}
-			
+
 		}
-		
-		if (check("RIGHT"))  {
-			
-			if(collide(SOLID, x + 3, y) != null) {
-				
+
+		if (check("RIGHT")) {
+
+			if (collide(SOLID, x + moveSpeed, y) != null) {
+
 				x += 0;
 			} else {
 				x += moveSpeed;
 			}
-			
+
 		}
-		
-		if(pressed("UP"))  {
+
+		if (pressed("UP")) {
 			boolean jumped = false;
 
 			// normal jump
 			if (onGround) {
-			speed.y = -jumpSpeed;
-			//jumpSnd.play();
-			jumped = true;
-			isJumping = true;			
+				speed.y = -jumpSpeed;
+				// jumpSnd.play();
+				jumped = true;
+				isJumping = true;
 			}
-			
+
 			if (isJumping) {
 				speed.y = -jumpSpeed;
-				//jumpSnd.play();
+				// jumpSnd.play();
 			}
-			
+
 		}
-		
+
 		gravity(delta);
-		
+
 		if (speed.y < 0 && !check("UP")) {
 			gravity(delta);
 			gravity(delta);
-			}
+		}
 	}
 }
